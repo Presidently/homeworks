82eea48002fc756964a8c3d1fefa02b0952aeea2
Presidently <Presidently@ukr.net>
2016-10-24 22:19:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8">
+    <title>Неназваный</title>
+    <link href ="files/css/reset.css" rel = "stylesheet" type = "text/css" />
+    <link href ="files/css/style.css" rel = "stylesheet" type = "text/css" />
+    <link href = 'files/css/nivo-slider.css' rel = 'stylesheet' type = 'text/css' />
+    <link href = 'files/js/owl.carousel/owl.carousel.css' rel = 'stylesheet' type = 'text/css' />
+
+    <script src = 'files/js/jquery-1.8.2.min.js'></script>
+    <script src = 'files/js/jquery.nivo.slider.pack.js'></script>
+    <script src = 'files/js/script.js'></script>
+    <script src = 'files/js/owl.carousel/owl.carousel.min.js'></script>
+
+
+
+</head>
+<body>
+
+<div id="top-fix-bar">
+    <div id="top-bar">
+    <ul>
+        <li><a>Главная</a></li>
+        <li><a>Контакты</a></li>
+        <li><a>Отзывы</a></li>
+    </ul>
+    </div>
+</div>
+<div id="main">
+
+
+    <div id="header">
+        <h1 id="title1" align ='center'>Luxury device</h1>
+        <h2 id="title2" align ='center'>Элитные штучки</h2>
+    </div>
+
+
+
+
+
+
+
+
+    <div id="content">
+
+        <div id ='slider'>
+            <div> <a> <img src="files/media/images_promo/b1.jpg" /> </a></div>
+            <div> <a> <img src="files/media/images_promo/b2.jpg" /> </a></div>
+        </div>
+        <h3 align="center" class ="titleh3">Коллекция новинок </h3>
+        <p align="center" id="category">
+            <a class="active" title="Для общуст-го назначение"> Для общуст-го назначение</a>
+            <a title="Для спецназначение"> Для спецназначение</a>
+        </p>
+        <div id="good-box">
+            <div class="good-box-item">
+                <p> ТекстТекст ТекстТекст ТекстТекст</p>
+            </div>
+            <div class="good-box-item">
+                <p> ТекстТекст ТекстТекст ТекстТекст888</p>
+            </div>
+            <div class="good-box-item">
+                <p>ggg ТекстТекст ТекстТекст ТекстТекст</p>
+            </div>
+            <div class="good-box-item">
+                <p>ggg ТекстТекст ТекстТекст ТекстТекст</p>
+            </div>
+            <div class="good-box-item">
+                <p>ggg ТекстТекст ТекстТекст ТекстТекст</p>
+            </div>
+            <div class="good-box-item">
+                <p>ggg ТекстТекст ТекстТекст ТекстТекст</p>
+            </div>
+        </div>
+
+<div id="goods-content">
+
+    <h3 align="center" class ="titleh3">Галерея</h3>
+
+    <div id="block-main-image">
+<div id="mini-gallery">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+
+    <span class="clear"></span>
+</div>
+<div id="main-photo"> </div>
+    </div>
+
+</div>
+
+
+    </div>
+
+
+</div>
+
+</body>
+</html>
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 28.09.2016
+ * Time: 22:27
+ */
+
+
+
+?>
